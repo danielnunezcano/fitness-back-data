a776939f06f9eb24de5049054728463e49a9d931
Daniel Nunez Cano <danielnunezcano@gmail.com>
2020-06-17 16:34:23 +0200

    first commit

---
+++ src/main/java/com/danielnc/fitnessbackdata/FitnessBackDataApplication.java
@@ -0,0 +1,13 @@
+package com.danielnc.fitnessbackdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessBackDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessBackDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielnc/fitnessbackdata/FitnessBackDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielnc.fitnessbackdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessBackDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
